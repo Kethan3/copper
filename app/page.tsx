8f5273749ada7f6665a9ab103f611e1bd698079d
Kethan3 <kethangowda003@gmail.com>
2025-04-16 15:19:16 +0530

    implement navigation bar and update root page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import NavigationBar from "./components/navigation-bar/NavigationBar";
 
 const RootPage = () => {
-  return <div>RootPage</div>;
+  return (
+    <div className="flex flex-col items-stretch">
+      <NavigationBar />
+      <div className="h-48 bg-orange-500"  />
+      <div  className="h-48 bg-purple-500"/>
+      <div  className="h-28 bg-green-500"/>
+    </div>
+  );
 };
 
 export default RootPage;
